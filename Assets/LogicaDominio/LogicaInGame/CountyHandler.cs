7eed81aae19f3d5862e7e5cee098b126e708d5d6
juanMediano <juanmediano2001@gmail.com>
2022-05-14 11:26:30 +0200

    para javi

--- Assets/LogicaDominio/LogicaInGame/CountyHandler.cs
+++ Assets/LogicaDominio/LogicaInGame/CountyHandler.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(PolygonCollider2D))] //Para que coja el contorno de cada pais para el cambio de color
 public class CountyHandler : MonoBehaviour
 {
-    //Tamaño (para el zoom)
-    private Vector3 tamano;
-
     private string id,propietario;
     private int numTropas;
     private List<string> TerrColindantes;
+
+    //Tamaño (para el zoom)
+    private Vector3 tamano;
+
+    private SpriteRenderer sprite;
     //void Start(){}
     //void Update(){}
     string getId(){
@@ -31,11 +33,25 @@ public class CountyHandler : MonoBehaviour
 
     void OnMouseEnter(){
         tamano = new Vector3(+10.0f,+10.0f,+0.0f);
-        transform.localScale += tamano;
+        transform.localScale += tamano;        
     }
 
     void OnMouseExit(){
         tamano = new Vector3(-10.0f,-10.0f,-0.0f);
         transform.localScale += tamano;
+    }
+
+    //void CambiarColor(){
+    void OnMouseClick(){
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color", Color.red);
+    }
+
+    //Nos pasan el color como parámetro según el jugador que posea el territorio
+    void CambiarColor(Color color){
+        sprite = GetComponent<SpriteRenderer>();
+        
+        //Color color = new Color(0.2F, 0.3F, 0.4F);
+        sprite.color = color;
     }
 }
